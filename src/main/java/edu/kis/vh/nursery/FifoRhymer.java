6871fbd92e7976cee1415d88a53a201b2fc6f629
PAWEL-W-STUDIA <251244@edu.p.lodz.pl>
2025-04-03 17:05:28 +0200

    PR checkpoint 2

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    // Zmienna 'temp' oznaczone jako final, ponieważ nie będziemy zmieniać referencji do tego obiektu
+    public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
